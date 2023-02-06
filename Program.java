f287c5766a5502a32b7b610f69a9754513d340ed
Rivaldo Ramos <ramosmendesfilho@hotmail.com>
2023-02-06 18:38:08 -0300

    Update calculator

--- Program.java
+++ Program.java
@@ -3,32 +3,44 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Digite um número: ");
-        double a = in.nextDouble();
-        System.out.print("Digite outro número: ");
-        double b = in.nextDouble();
+        boolean verification = false;
 
-        System.out.println("ESCOLHA A OPERAÇÃO: \n1 - SOMA\n2 - SUBTRAÇÃO\n3 - MULTIPLICAÇÃO\n4 - DIVISÃO");
-        int escolha = in.nextInt();
+        while (verification == false) {
+            System.out.print("Digite um número: ");
+            double a = in.nextDouble();
+            System.out.print("Digite outro número: ");
+            double b = in.nextDouble();
 
-        switch (escolha) {
-            case 1:
-                System.out.println(sum(a,b));
-                break;
-            case 2:
-                System.out.println(subtraction(a, b));
-                break;
-            case 3:
-                System.out.println(multiplication(a,b));
-                break;
-            case 4:
-                System.out.println(division(a,b));
-                break;
-            default:
-                System.out.println("Ocorrência não encontrada");
-                break;
+            System.out.println("ESCOLHA A OPERAÇÃO: \n1 - SOMA\n2 - SUBTRAÇÃO\n3 - MULTIPLICAÇÃO\n4 - DIVISÃO");
+            int choice = in.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.printf("%.2f", sum(a, b));
+                    break;
+                case 2:
+                    System.out.printf("%.2f", subtraction(a, b));
+                    break;
+                case 3:
+                    System.out.printf("%.2f", multiplication(a, b));
+                    break;
+                case 4:
+                    System.out.printf("%.2f", division(a, b));
+                    break;
+                default:
+                    System.out.println("Ocorrência não encontrada");
+                    break;
+            }
+
+            char name = 'a';
+            while (name != 's' && name != 'n') {
+                System.out.println("Deseja continuar? (s/n)");
+                name = in.next().charAt(0);
+            }
+            verification = name == 'n';
+
+
         }
-
         in.close();
     }
 
